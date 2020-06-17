726d8db59b4d8cddcf29730dd08a4e057ca654eb
david-wei0628 <62085333+david-wei0628@users.noreply.github.com>
2020-06-17 15:07:06 +0800

    HTMLDOW

---
+++ innovue-test/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace innovue_test
+{
+    partial class WorkTwo
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.html2xml = new System.Windows.Forms.Button();
+            this.xml2html = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(571, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "下載";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // html2xml
+            // 
+            this.html2xml.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.html2xml.Location = new System.Drawing.Point(18, 226);
+            this.html2xml.Name = "html2xml";
+            this.html2xml.Size = new System.Drawing.Size(140, 65);
+            this.html2xml.TabIndex = 1;
+            this.html2xml.Text = "HTMLtoXML";
+            this.html2xml.UseVisualStyleBackColor = true;
+            this.html2xml.Click += new System.EventHandler(this.html2xml_Click);
+            // 
+            // xml2html
+            // 
+            this.xml2html.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.xml2html.Location = new System.Drawing.Point(571, 226);
+            this.xml2html.Name = "xml2html";
+            this.xml2html.Size = new System.Drawing.Size(140, 65);
+            this.xml2html.TabIndex = 2;
+            this.xml2html.Text = "XMLtoHTML";
+            this.xml2html.UseVisualStyleBackColor = true;
+            this.xml2html.Click += new System.EventHandler(this.xml2html_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "檔名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "網址";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(482, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // WorkTwo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.xml2html);
+            this.Controls.Add(this.html2xml);
+            this.Controls.Add(this.button1);
+            this.Name = "WorkTwo";
+            this.Text = "WorkTwo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button html2xml;
+        private System.Windows.Forms.Button xml2html;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ innovue-test/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace innovue_test
+{
+    public partial class WorkTwo : Form
+    {
+        public WorkTwo()
+        {
+            InitializeComponent();
+            textBox1.Text = "6001234";
+            textBox2.Text = "https://www.google.com.tw/";
+        }
+
+        public static string urlx = null;
+        public static MatchCollection matches;
+        public static string xpr = null;//正則後暫存用
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient url = new WebClient();
+            url.DownloadFile(textBox2.Text, textBox1.Text + ".html");
+        }
+
+        private void html2xml_Click(object sender, EventArgs e)
+        {
+            urlx = textBox1.Text;
+
+            string path =   urlx + ".html";  //<相對路徑的html檔
+            //Console.WriteLine(path);
+            if (!File.Exists(path))
+            {
+                MessageBox.Show(path + " 檔案不存在");
+            }
+            else
+            {
+                StreamReader streamReader = new StreamReader(path, Encoding.GetEncoding("utf-8"));
+                string content = streamReader.ReadToEnd();
+                //MessageBox.Show(content);
+
+
+                //string pattern = "<TITLE>[a-zA-Z0-9 !-@]+" + "</TITLE>";
+                ////MatchCollection matches;
+
+                //Regex defaultRegex = new Regex(pattern);
+
+                ////Get matches of pattern in text
+                //matches = defaultRegex.Matches(content);
+
+                //foreach (Match ctr in matches)
+                //{
+                //    MatchCollection matches2;
+                //    string pattern2 = @"[0-9]+";
+                //    defaultRegex = new Regex(pattern2);
+                //    matches2 = defaultRegex.Matches(ctr.Value);
+                //    //Console.WriteLine(ctr.Value);
+                //    foreach (Match ctr2 in matches2)
+                //    {
+                //        Console.WriteLine(ctr2);
+                //        xpr = ctr2.Value;
+                //    }
+                //}//HTML段落正則，再取得需要的內容
+
+                //pattern = @"[0-9]+";
+                //defaultRegex = new Regex(pattern);
+                //matches = defaultRegex.Matches(content);
+                //foreach(Match ctr in matches)
+                //{
+                //    Console.WriteLine(ctr);
+
+                //}
+
+
+                String patten = @"[a-z_0-9]+" + ".png";
+                Regex defaultRegex = new Regex(patten);
+                matches = defaultRegex.Matches(content);
+                //MessageBox.Show(content);
+                foreach(Match ctr in matches)
+                {
+                    //Console.WriteLine(ctr);
+                    MessageBox.Show(ctr.Value);
+
+                }
+
+                streamReader.Close();
+                streamReader.Dispose();//關閉使用中的HTML
+
+                //XDocument doc = new XDocument();
+
+                //doc = new XDocument(
+                //    new XElement("PatentInfo",
+                //        new XElement("PN", xpr)
+                //    )
+                //);//編碼成XML格式
+
+                //doc.Declaration = new XDeclaration("1.0", "utf-8", "true");//編碼格式
+                //doc.Save(textBox1.Text + ".xml");//存檔
+
+
+            }
+         }
+
+        private void xml2html_Click(object sender, EventArgs e)
+        {
+            urlx = textBox1.Text;
+
+            string path = urlx + ".xml";  //<相對路徑的html檔
+            Console.WriteLine(File.Exists(path));
+            if (!File.Exists(path))
+            { MessageBox.Show(path + "檔案不存在"); }
+            else
+            {
+
+
+            }
+        }
+    }
+}
